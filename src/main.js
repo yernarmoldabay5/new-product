026b1550d3bfaeacfca338f7d32867c1509201d6
yernarmoldabay5 <68390376+yernarmoldabay5@users.noreply.github.com>
2023-05-04 14:18:35 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 let shop = document.getElementById("shop");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
-let tg = window.Telegram.WebApp;
-
- tg.expand(); //расширяем на все окно  
-   tg.MainButton.text = "Changed Text"; //изменяем текст кнопки 
-   tg.MainButton.setText("Changed Text1"); //изменяем текст кнопки иначе
-   tg.MainButton.textColor = "#F55353"; //изменяем цвет текста кнопки
-   tg.MainButton.color = "#143F6B"; //изменяем цвет бэкграунда кнопки
-   tg.MainButton.setParams({"color": "#143F6B"}); //так изменяются все параметры
-
-//Telegram.WebApp.onEvent('mainButtonClicked', function(){
- // window.location.href = 'cart.html';
-//});
 
 let generateShopItems = () => {
   return (shop.innerHTML = shopItems
